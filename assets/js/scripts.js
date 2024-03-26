659ff1f639b5b47820c1899c79712943a5ba127b
Rafael dos Santos <rspwebdesigner@gmail.com>
2024-03-26 07:30:32 -0300

    mudança geral

---
+++ assets/js/scripts.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.influencers', {
+    navigation: {
+        nextEl: '.influencers .swiper-button-next',
+        prevEl: '.influencers .swiper-button-prev',
+    },
+});
+
+const swiper2 = new Swiper('.como-usar', {
+    navigation: {
+        nextEl: '.como-usar .swiper-button-next',
+        prevEl: '.como-usar .swiper-button-prev',
+    },
+});
